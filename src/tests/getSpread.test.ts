20ba043110a01246f8ad1e5909ca9b64ecb18d89
Nicholas Morrow <njmorrow95@gmail.com>
2020-01-02 21:27:30 -0500

    Add test for 01001.

--- src/tests/getSpread.test.ts
+++ src/tests/getSpread.test.ts
@@ -7,6 +7,10 @@ describe('getSpread', () => {
 	it('handles 01010', () => {
 		expect(getSpread([false, true, false, true, false])).toBe(1.5);
 	});
+
+	it('handles 01001', () => {
+		expect(getSpread([false, true, false, false, true])).toBbe(3.5);
+	});
 });
 
 [];
