f22937cbdca26c374844b2e2149777e72ae571a8
Nicholas Morrow <njmorrow95@gmail.com>
2020-01-08 20:17:07 -0500

    Change getSpread to take into account groups and to 'loop around' instead of asserting bookends.

--- src/utilities/getAddedEntriesForMostEvenSelection.ts
+++ src/utilities/getAddedEntriesForMostEvenSelection.ts
@@ -1,7 +1,6 @@
 import { getMostEvenSelection } from 'utilities/getMostEvenSelection';
 
 export const getAddedEntriesForMostEvenSelection = (input: boolean[], numDates: number): boolean[] => {
-	// TODO: Reconsider whether it makes sense to bookend.
 	input[0] = true;
 	input[input.length - 1] = true;
 	const mostEvenSelection = getMostEvenSelection(input, numDates);
--- src/utilities/getSpread.ts
+++ src/utilities/getSpread.ts
@@ -1,6 +1,15 @@
 import { Throw } from 'utilities/Throw';
 
+/**
+ * Iterate through the distribution from the right and left.
+ * Record how far away each group of entries is from other groups.
+ * @param distribution
+ */
 export const getSpread = (distribution: boolean[]): number => {
+	return 1 / getStdDev(getGaps(distribution));
+};
+
+const getGaps = (distribution: boolean[]): number[] => {
 	const numAssignments = distribution.reduce((agg, cur) => {
 		if (cur) {
 			agg += 1;
@@ -8,33 +17,67 @@ export const getSpread = (distribution: boolean[]): number => {
 		return agg;
 	}, 0);
 
-	if (numAssignments > 1) {
-		Throw.InvalidIf(
-			!(distribution[0] && distribution[distribution.length - 1]),
-			`Because numAssignments (${numAssignments}) was greater than 1, expected distribution to have assignments at start and finish, but it did not. Distribution: ${distribution}.`,
-		);
-	}
+	Throw.InvalidIf(numAssignments <= 1, `Number of assignments must be greater than 1, but was ${numAssignments}`);
 
 	const gaps: number[] = [];
-	let lastSeenIndex = 0;
 
-	distribution.forEach((value, index) => {
-		if (value) {
-			gaps.push(index - lastSeenIndex);
-			lastSeenIndex = index;
+	let distanceFromLeft = 0;
+
+	for (let i = 0; i < distribution.length; i++) {
+		if (distribution[i]) {
+			break;
 		}
-	});
+		distanceFromLeft += 1;
+	}
 
-	lastSeenIndex = 0;
+	let distanceFromRight = 0;
 
-	[...distribution].reverse().forEach((value, index) => {
-		if (value) {
-			gaps.push(index - lastSeenIndex);
-			lastSeenIndex = index;
+	for (let i = distribution.length; i >= 0; i--) {
+		if (distribution[i]) {
+			break;
 		}
-	});
+		distanceFromRight += 1;
+	}
 
-	return 1 / getStdDev(gaps);
+	for (let i = 0; i < distribution.length; i++) {
+		if (distribution[i] && !distribution[i - 1]) {
+			let gapCounter = 0;
+			let currentIndex = i;
+			while (true) {
+				gapCounter += 1;
+				currentIndex -= 1;
+				if (distribution[currentIndex]) {
+					break;
+				}
+				if (i === 0) {
+					gapCounter += distanceFromRight;
+					break;
+				}
+			}
+			gaps.push(gapCounter);
+		}
+	}
+
+	for (let i = distribution.length; i >= 0; i--) {
+		if (distribution[i] && !distribution[i + 1]) {
+			let gapCounter = 0;
+			let currentIndex = i;
+			while (true) {
+				gapCounter += 1;
+				currentIndex += 1;
+				if (distribution[currentIndex]) {
+					break;
+				}
+				if (i === distribution.length) {
+					gapCounter += distanceFromLeft;
+					break;
+				}
+			}
+			gaps.push(gapCounter);
+		}
+	}
+
+	return gaps;
 };
 
 const getStdDev = (array: number[]) => {
