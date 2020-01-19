197c17c3119af7806b883069220e25107b64196f
Nicholas Morrow <njmorrow95@gmail.com>
2020-01-18 23:43:31 -0500

    For netlify

--- client/src/utilities/getBaseUrl.ts
+++ client/src/utilities/getBaseUrl.ts
@@ -1,5 +1,5 @@
 const developmentUrl = 'http://localhost:3001';
-const productionUrl = '';
+const productionUrl = 'https://sleepy-tundra-74114.herokuapp.com/';
 
 export const getBaseUrl = () => {
 	const env = process.env.NODE_ENV;
