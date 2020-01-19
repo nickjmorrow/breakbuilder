49b151f999105c3f66dea2d4399db3b1efb9fb3e
Nicholas Morrow <njmorrow95@gmail.com>
2020-01-18 23:56:16 -0500

    Add build folder for netlify.

---
+++ client/build/precache-manifest.4e9856c967021b247c3f3cbb81c7c677.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b474e2482e5169030dda7f5b0aee19c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80d957e785604607f72d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e8d159638db6e49f8548",
+    "url": "/static/js/2.8c134976.chunk.js"
+  },
+  {
+    "revision": "72ccebc24f8112d32df87d43660feeff",
+    "url": "/static/js/2.8c134976.chunk.js.LICENSE"
+  },
+  {
+    "revision": "80d957e785604607f72d",
+    "url": "/static/js/main.1b5fb3e9.chunk.js"
+  },
+  {
+    "revision": "79e93a0f84b38c02b556",
+    "url": "/static/js/runtime-main.0c4ccf2e.js"
+  }
+]);
